7d8b2fb43dba13e6917e19ff670dbb7a64830c67
Iulik <inicula1989@gmail.com>
2024-03-02 13:07:07 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,86 @@
+// var testVariable = 5 //number
+// let str = "let's" //string
+// let bool = false //boolean
+// let unde = undefined //undefiend
+// let testNull = null //null
+// let testBigInt = BigInt(999) //bigInt
+// let symb = Symbol('') //symbol
+
+
+
+
+// const obj = {
+//     nume: 'Iulian',
+//     hobby: 'swiming'
+// }  //object
+
+// const arr = [555, 'str', false, {}, []]
+
+// const func = () => {}
+
+
+
+// let num1 = 7
+// let num2 = 6
+
+// let num3 = num2
+
+// num2 = 5
+
+// const obj1 = { name: 'Iulian'}
+// const obj2 = obj1
+
+// obj1.name = 'Alex'
+// obj1.friend ='Victor'
+
+// console.log(typeof obj1)
+
+// let num1 = 3
+// let num2 = 4
+
+// console.log(num1 + num2)
+// console.log(num1 - num2)
+// console.log(num1 / num2)
+// console.log(num1 * num2)
+
+// num1--
+
+// num1 = num1 - 1
+
+// num1++
+
+// num1 = num1 + 1
+
+// num1 = num1 + 5
+
+// num1+=num2
+
+// num1-=2
+
+
+// console.log(3**3)
+
+// const bool = false
+
+// console.log(!bool)
+
+// const str = '89'
+// const str1 = 1
+
+let num1 = 4
+let num2 = 2 + 2
+
+// console.log(num1 < num2)
+// console.log(num1 >= num2)
+// console.log(num1 <= num2)
+
+// console.log(num1 == num2)
+
+// console.log(1 == true)
+console.log(num1 === num2)
+
+
+
+
+
+
